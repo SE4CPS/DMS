3c1cdb5a4c2cb40b9a55c444534e63b0e93593d1
VongChang <v_chang9@u.pacific.edu>
2025-02-24 14:20:20 -0800

    change table name to team-4

--- project/part1/team-4/Web/app.py
+++ project/part1/team-4/Web/app.py
@@ -4,6 +4,32 @@ import psycopg2
 app = Flask(__name__)
 DATABASE_URL = "postgresql://neondb_owner:npg_M5sVheSzQLv4@ep-shrill-tree-a819xf7v-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
 
+try:
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(DATABASE_URL)
+    conn.autocommit = True  # Enable auto-commit for transactions
+    print("Connected to PostgreSQL successfully!")
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+# --- CREATE TABLES ---
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS team4_flowers (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            last_watered DATE NOT NULL,
+            water_level INT NOT NULL,
+            min_water_required INT NOT NULL
+        );
+    """)
+
+# --- UPDATE Water Level ---
+    #curr.execute("""()""")
+
+except Exception as e:
+    print("Error:", e)
+
 def get_db_connection():
     return psycopg2.connect(DATABASE_URL)
 
@@ -15,7 +41,7 @@ def index():
 def manage_flowers():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM flowers")
+    cur.execute("SELECT * FROM team4_flowers")
     flowers = cur.fetchall()
     cur.close()
     conn.close()
@@ -29,7 +55,7 @@ def add_flower():
     stock = request.form['stock']
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO flowers (name, color, price, stock) VALUES (%s, %s, %s, %s)", (name, color, price, stock))
+    cur.execute("INSERT INTO team4_flowers (name, color, price, stock) VALUES (%s, %s, %s, %s)", (name, color, price, stock))
     conn.commit()
     cur.close()
     conn.close()
@@ -39,7 +65,7 @@ def add_flower():
 def delete_flower(flower_id):
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("DELETE FROM flowers WHERE flower_id = %s", (flower_id,))
+    cur.execute("DELETE FROM team4_flowers WHERE flower_id = %s", (flower_id,))
     conn.commit()
     cur.close()
     conn.close()
