203599540a5e5001c4a503070d33c2c7daba415f
Fernando Villasenor <148602612+fernalga@users.noreply.github.com>
2025-03-11 18:55:36 -0700

    create & insert data

---
+++ project/part1/team-2/createDB.sql
@@ -0,0 +1,14 @@
+-- Create data
+CREATE TABLE team2_flowers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    last_watered DATE NOT NULL,
+    water_level INT NOT NULL,
+    min_water_required INT NOT NULL
+);
+-- Insert data
+INSERT INTO team2_flowers (name, last_watered, water_level, min_water_required) 
+VALUES 
+('Rose', '2024-02-10', 20, 5),
+('Tulip', '2024-02-08', 10, 7),
+('Lily', '2024-02-05', 3, 5);
