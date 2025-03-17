711510cec3a8b4d376f52c77e9f3f26185ae5675
DeltaYankee86 <d_yang19@u.pacific.edu>
2025-03-17 09:47:49 -0700

    team-1: updated imported postgress_crud to postgress_ping to call db connection function

--- project/part1/team-1/PostgreSQL/postgress_ping.py
+++ project/part1/team-1/PostgreSQL/postgress_ping.py
@@ -1,14 +1,30 @@
 from dotenv import load_dotenv, dotenv_values
-import psycopg2
-import os
+import psycopg2, sys, os
 
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+# import postgress-crud.py into app.py
+import postgress_crud as db
+
+'''
 # Input the absolute path to the .env file
 load_dotenv()
 
 # AWS PostgreSQL connection
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URI')}:5432/{os.getenv('DB_NAME')}"
-
+'''
 try:
+    conn = db.get_db_connection()
+    print("Connected to PostgreSQL successfully!")
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    db_version = cur.fetchone()
+    print("PostgreSQL Version:", db_version)
+    cur.close()
+    conn.close()
+    print("Connection closed.")
+    
+    '''
     # Connect to PostgreSQL
     conn = psycopg2.connect(DATABASE_URL)
     print("Connected to PostgreSQL successfully!")
@@ -25,6 +41,7 @@ try:
     cur.close()
     conn.close()
     print("Connection closed.")
+    '''
 
 except Exception as e:
     print("Error connecting to PostgreSQL:", e)
