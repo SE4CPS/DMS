a8a50adbf668bdd6ae5d5ad880d1db2c2f6304fa
Ngoc Gia Han Nguyen <n_nguyen134@u.pacific.edu>
2025-03-19 18:39:04 -0700

    Update README.md, project structure, database connection

---
+++ project/part1/team-3/database/db_connection.py
@@ -0,0 +1,18 @@
+import psycopg2
+import os
+from dotenv import load_dotenv  # Create .env to store db url 
+                                # Enhance security
+
+# Load env variables
+load_dotenv()
+
+# Get db connection details from env var
+db_url=os.getenv("database_url")
+
+
+def get_db_connection():
+    try:
+        return psycopg2.connect(db_url)
+    except Exception as e:
+        print("Error connecting to database: ", e)
+        return None
