54ac97ca65ccae1b1149a73635c61f3fdedc6711
K5741 <k_amador@u.pacific.edu>
2025-04-20 23:17:04 -0700

    Testing, and inserted customer data

--- project/part2/team-7/app.py
+++ project/part2/team-7/app.py
@@ -240,6 +240,15 @@ def fast_query():
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
 
+    #Tests
+    cur.execute("SELECT COUNT(*) FROM team7_orders")
+    order_count = cur.fetchone()[0]
+    cur.execute("SELECT COUNT(*) FROM team7_customers")
+    customer_count = cur.fetchone()[0]
+
+    if order_count == 0 or customer_count == 0:
+        return jsonify({"message": "No data in orders or customers"})
+    
     # FAST: efficient join with LIMIT and only recent orders
     cur.execute("""
         SELECT 
