b1fae002fc46e6f99a235f940738c1b5047c6180
Ngoc Gia Han Nguyen <n_nguyen134@u.pacific.edu>
2025-04-21 14:22:51 -0700

    uncommented delete...

--- project/part2/team-3/app.py
+++ project/part2/team-3/app.py
@@ -143,18 +143,18 @@ def update_flower():
     return jsonify({"message": "Flower updated successfully!"})
 
 # Delete a flower by ID
-# @app.route('/flowers/<int:id>', methods=['DELETE'])
-# def delete_flower(id):
-#     conn = get_db_connection()
-#     cur = conn.cursor()
-#     cur.execute(
-#         """
-#         DELETE FROM team3_flowers WHERE flower_id = %s
-#         """, (id,))
-#     conn.commit()
-#     cur.close()
-#     conn.close()
-#     return jsonify({"message": "Flower deleted successfully!"})  
+@app.route('/flowers/<int:id>', methods=['DELETE'])
+def delete_flower(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute(
+        """
+        DELETE FROM team3_flowers WHERE flower_id = %s
+        """, (id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({"message": "Flower deleted successfully!"})  
   
 
 # Slow query that joins customers and orders with decryption
